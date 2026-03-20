38ad8644429422b8d883f761eca989a8dbd56ac8
Anantha-Shayan <ananthashayan2802@outlook.com>
2026-03-20 11:53:43 +0530

    Unoptimized version with latency time calculation codes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 from resume_parser import pdf_resume, docx_resume
 from vectorize import search_jobs
+import time 
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ def home():
 
 @app.route('/analyze', methods = ['POST'])
 def analyse():
+    total_start = time.perf_counter()
     uploaded_file = request.files["resume"]
 
     if uploaded_file.filename == "":
@@ -27,6 +29,7 @@ def analyse():
     ext = (uploaded_file.filename).split(".")[-1].lower()
 
     with open(file_path, 'rb') as f:
+        extract_start = time.perf_counter()
         if ext=='pdf':
           text, headings, sections, images, bboxes = pdf_resume(f)
 
@@ -35,14 +38,31 @@ def analyse():
         
         else:
            return "Unsupported file type!"
+        extract_end = time.perf_counter()
         
+        
+    match_start = time.perf_counter()
     matches = search_jobs(sections,text)
+    match_end = time.perf_counter()
 
-    return render_template(
+    render_start = time.perf_counter()
+    response = render_template(
        'job.html',
        jobs = matches,
        sections = sections
-    )
+       )
+    
+    render_end = time.perf_counter()
+    total_end = time.perf_counter()
+
+    #latency check
+    print(f"Resume parsing time: {(extract_end-extract_start)*1000:.3f} ms")
+    print(f"Search stage time: {(match_end-match_start)*1000:.3f} ms")
+    print(f"Render time: {(render_end-render_start)*1000:.3f} ms")
+    print(f"Full pipeline time: {(total_end-total_start)*1000:.3f} ms")
+
+    return response
+
 
 
 if __name__=='__main__':
